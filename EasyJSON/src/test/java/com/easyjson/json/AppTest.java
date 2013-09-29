203d80888c94f70c52a9dd8db43cc0eebe669eef
Andrea Cosentino <ancosen@gmail.com>
2013-09-29 15:42:59 +0200

    Primi elementi

--- EasyJSON/src/test/java/com/easyjson/json/AppTest.java
+++ EasyJSON/src/test/java/com/easyjson/json/AppTest.java
@@ -1,38 +1,20 @@
-package JSON;
+package com.easyjson.json;
 
-import junit.framework.Test;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import com.easyjson.json.JSONObject;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public void test1() throws IOException {
+		StringWriter out = new StringWriter();
+		Map map = new HashMap();
+		JSONObject.writeJSONString(map, out);
+		System.out.println(out.toString());
+	}
 }
